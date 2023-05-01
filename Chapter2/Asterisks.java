8855895859436427d9296888b409be1f34ad58ae
Peter Joy <126507575+Joypeterscholar@users.noreply.github.com>
2023-05-01 06:12:27 -0400

    Add files via upload

---
+++ Chapter2/Asterisks.java
@@ -0,0 +1,30 @@
+public class Asterisks{
+		public static void main(String[] args){
+			println("*********");
+			println("*              *");
+			println("*              *");
+			println("*              *");
+			println("*              *");
+			println("*              *");
+			println("*              *");
+			println("*              *");
+			println("*********");
+
+			println(“         ***       ”);
+			println(“    *            *”);
+			println(“  *                *”);
+			println(“  *                *”);
+			println(“  *                *”);
+			println(“  *                *”);
+			println(“    *            *");
+			println("         ***       ");
+
+			println("         *       ");
+			println("       ***     ");
+			println("     *****   ");
+			println("         *       ");
+			println("         *       ");
+			println("         *       ");
+			println("         *       ");
+		}
+	}
